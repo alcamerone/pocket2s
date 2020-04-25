3b05b6c2e5115ab52ebd42641635077a1362c874
alcamerone <al.camerone@gmail.com>
2020-04-25 13:53:23 +1200

    Add licence agreement and copyright statements

--- randSource/concurrencySafeSource.go
+++ randSource/concurrencySafeSource.go
@@ -1,3 +1,20 @@
+/*    package "randSource" provides random number sources for the Pocket2s server.
+ *    Copyright (C) 2020 Cameron Ekblad.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package randSource
 
 import (
